adc291f4204cb6149e9d3f86098605411676bfde
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-08-18 20:40:26 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/next.config.ts
+++ src/next.config.ts
@@ -8,6 +8,12 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { isServer }) => {
+    // This is to fix a build error for `handlebars` which is a dependency of genkit.
+    // @see https://github.com/firebase/genkit/issues/323
+    config.resolve.alias.handlebars = 'handlebars/dist/handlebars.min.js';
+    return config;
+  },
 };
 
 export default nextConfig;
