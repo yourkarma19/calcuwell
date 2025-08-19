421a17ba4fb9790167b37d393679e1d94359d344
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-08-19 16:00:56 +0000

    can i go live now ?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import Footer from "@/components/layout/footer";
 import "./globals.css";
 import { headers } from 'next/headers';
 import { cn } from "@/lib/utils";
+import { GoogleAnalytics } from '@next/third-party/google'
 
 const inter = Inter({
   subsets: ["latin"],
@@ -85,6 +86,7 @@ export default async function RootLayout({
           <Footer />
           <Toaster />
         </ThemeProvider>
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || ""} />
       </body>
     </html>
   );
