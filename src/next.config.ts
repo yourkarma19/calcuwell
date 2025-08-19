55aabb9e08d0fc584202b2e90e86c530d81334bc
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-08-19 13:56:58 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -13,6 +14,13 @@ const nextConfig: NextConfig = {
     // @see https://github.com/firebase/genkit/issues/323
     config.resolve.alias.handlebars = 'handlebars/dist/handlebars.min.js';
     return config;
+  },
+  devIndicators: {
+    // This is to fix a warning that shows up in the development environment.
+    // @see https://nextjs.org/docs/app/api-reference/config/next-config-js/allowedDevOrigins
+    allowedDevOrigins: [
+        'https://*.cloudworkstations.dev',
+    ]
   },
 };
 
