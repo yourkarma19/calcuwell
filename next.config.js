40c76351c7ebc43d9fae6b694921a01b2058956d
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-08-20 09:54:09 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -7,12 +7,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config, { isServer }) => {
-    // This is to fix a build error for `handlebars` which is a dependency of genkit.
-    // @see https://github.com/firebase/genkit/issues/323
-    config.resolve.alias.handlebars = 'handlebars/dist/handlebars.min.js';
-    return config;
-  },
   async headers() {
     return [
       {
