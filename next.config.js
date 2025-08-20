01c8ecbc1dcbf03d93f0d5de424bdc12e33ec052
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-08-20 09:56:45 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -1,33 +1,11 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // config options here
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-        ],
-      },
-    ];
   },
 };
 
