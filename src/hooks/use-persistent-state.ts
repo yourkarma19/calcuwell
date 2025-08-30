74d324a983f97aedee29b062089d78e1dbc3ea0a
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-08-30 20:52:56 +0000

    2:17:30 AM: Netlify Build

--- src/hooks/use-persistent-state.ts
+++ src/hooks/use-persistent-state.ts
@@ -6,7 +6,7 @@ import { useState, useEffect, Dispatch, SetStateAction } from "react";
 function usePersistentState<T>(
   key: string,
   defaultValue: T,
-  reviver?: (value: any) => T,
+  reviver?: (value: unknown) => T,
 ): [T, Dispatch<SetStateAction<T>>] {
   const [state, setState] = useState<T>(() => {
     try {
