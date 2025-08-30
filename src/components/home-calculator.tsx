02174e16be0fbddc0e9b2a3fce188df7005a6e1b
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-08-30 19:15:40 +0000

    *"I am editing my code in Hostinger and deploying it on Netlify. I want

--- src/components/home-calculator.tsx
+++ src/components/home-calculator.tsx
@@ -1,6 +1,13 @@
 
+"use client";
+
+import React from "react";
 import BasicCalculator from "@/components/calculator/basic-calculator";
 
-export default function HomeCalculator() {
+function HomeCalculator() {
     return <BasicCalculator />;
 }
+
+// Memoize the component to prevent re-renders when other parts of the homepage update.
+// This is a performance best practice for complex, stateful components.
+export default React.memo(HomeCalculator);
