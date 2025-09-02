384af85fb165a8afae9050f3ecc2123fa1c23393
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-09-02 13:35:52 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.js
@@ -0,0 +1,10 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    typescript: {
+        // Tell Next.js to use our custom tsconfig for builds
+        tsconfigPath: './tsconfig.src.json',
+    },
+};
+
+export default nextConfig;
