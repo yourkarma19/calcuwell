b914b8c3f6bf238cebed17e2da6ed0e895cdc92a
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-09-02 13:31:25 +0000

    check agin on this and make it clear The Netlify deploy errored, with th

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     typescript: {
         // Tell Next.js to use our custom tsconfig for builds
         tsconfigPath: './tsconfig.src.json',
     },
+    webpack: (config, { isServer }) => {
+        config.module.rules.push({
+            test: /\.ts$/,
+            use: 'ts-loader',
+        });
+        return config;
+    },
+    experimental: {
+        // This is to allow requests from the development environment
+        allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+    }
 };
 
 export default nextConfig;
