a84ffd7dc7c1e0b3b0a8fddbf8838ffad4a5c504
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-09-05 08:16:27 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,8 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     typescript: {
         // Tell Next.js to use our custom tsconfig for builds
         tsconfigPath: './tsconfig.src.json',
-    },
-    webpack: (config, { isServer }) => {
-        config.module.rules.push({
-            test: /\.ts$/,
-            use: 'ts-loader',
-        });
-        return config;
     },
 };
 
