033764806c87ded43fe781e82a19bb7fc3dac655
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-09-08 09:48:26 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,12 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        allowedDevOrigins: [
+            "http://localhost:3000",
+            "https://calcpro.online",
+        ]
+    },
     typescript: {
         // Tell Next.js to use our custom tsconfig for builds
         tsconfigPath: './tsconfig.src.json',
