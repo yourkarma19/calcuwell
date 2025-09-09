7043a77114adb5ff9050d5481f300706bfc8894e
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-09-09 14:20:03 +0000

    This report is a helpful tip for your development process, but it is **n

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-      },
-    ],
-  },
+  productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
