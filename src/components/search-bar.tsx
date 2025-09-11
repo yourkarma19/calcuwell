950234af891cba53a36b071027d9c7f7e805e614
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-09-11 14:48:45 +0000

    the ssearch bar colour combination is not looking good

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -97,7 +97,7 @@ export function SearchBar() {
         </Button>
       </PopoverTrigger>
       <PopoverContent
-        className="w-[var(--radix-popover-trigger-width)] p-0"
+        className="w-[var(--radix-popover-trigger-width)] p-0 border-0 shadow-lg"
         align="start"
         onOpenAutoFocus={() => inputRef.current?.focus()}
       >
