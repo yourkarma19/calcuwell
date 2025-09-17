c16dd5f2e15515ce2e6d02a6b826c473a16940e8
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-09-17 09:01:18 +0000

    Excellent, your app is live\! This is a very common and often frustratin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,14 +92,15 @@ export default function RootLayout({
         {/* Google tag (gtag.js) */}
         <Script
           strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17483796549"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}`}
         />
         <Script id="gtag-init" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-17483796549');
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ADS_ID}');
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
           `}
         </Script>
         <Script
