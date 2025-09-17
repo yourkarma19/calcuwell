e62c85b39cd976a968ca6b4d298684c35a020ed2
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-09-17 08:30:39 +0000

    make sure that all the items are load instantly

--- src/components/IconWrapper.tsx
+++ src/components/IconWrapper.tsx
@@ -3,28 +3,14 @@
 import { type LucideProps } from "lucide-react";
 import dynamic from "next/dynamic";
 import { Skeleton } from "@/components/ui/skeleton";
+import * as allIcons from "lucide-react";
 
 interface IconWrapperProps extends LucideProps {
   iconName: string;
 }
 
 const IconWrapper = ({ iconName, ...props }: IconWrapperProps) => {
-  const LucideIcon = dynamic(
-    () =>
-      import("lucide-react").then((mod) => {
-        const Icon = mod[iconName as keyof typeof mod] as React.FC<
-          LucideProps
-        >;
-        if (!Icon) {
-          // Fallback to a default icon if the requested icon is not found
-          return mod.HelpCircle;
-        }
-        return Icon;
-      }),
-    {
-      loading: () => <Skeleton className="h-6 w-6" />,
-    },
-  );
+  const LucideIcon = (allIcons as any)[iconName] ?? allIcons.HelpCircle;
 
   return <LucideIcon {...props} />;
 };
--- src/components/calculator/IconLoader.tsx
+++ src/components/calculator/IconLoader.tsx
@@ -1,27 +1,14 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import { LucideProps, Calculator } from "lucide-react";
+import * as allIcons from "lucide-react";
 
 interface IconLoaderProps extends LucideProps {
   iconName: string;
 }
 
 const IconLoader = ({ iconName, ...props }: IconLoaderProps) => {
-  const LucideIcon = dynamic(
-    () =>
-      import("lucide-react").then((mod) => {
-        const Icon = mod[iconName as keyof typeof mod];
-        // If the icon is not found in lucide-react, return the Calculator icon as a fallback.
-        if (!Icon) {
-          return Calculator;
-        }
-        return Icon as React.FC<LucideProps>;
-      }),
-    {
-      ssr: false, // Ensure this component is only rendered on the client side.
-    },
-  );
+  const LucideIcon = (allIcons as any)[iconName] ?? Calculator;
 
   return <LucideIcon {...props} />;
 };
