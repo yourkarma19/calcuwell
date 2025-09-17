95953840b9cf5cf310a01c7e3a90b63684f16027
Patel Karmkumar <patelkarmkumar19@gmail.com>
2025-09-17 10:10:23 +0000

    Copy and paste the AdSense code in between the <head></head> tags of you

--- src/components/analytics/Adsense.tsx
+++ src/components/analytics/Adsense.tsx
@@ -6,17 +6,11 @@ export default function Adsense() {
   if (process.env.NODE_ENV !== "production") {
     return null;
   }
-  if (!process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID) {
-    console.warn(
-      "Google Ads Client ID is not set. Ads will not be displayed.",
-    );
-    return null;
-  }
 
   return (
     <Script
       async
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1952235305826490"
       crossOrigin="anonymous"
       strategy="lazyOnload"
     />
